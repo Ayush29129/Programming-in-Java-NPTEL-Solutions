f63a9d63e152703987a3337419f7db82fad461f3
Ayush Gupta <48252204+Ayush29129@users.noreply.github.com>
2020-03-27 00:25:35 +0530

    Add files via upload

---
+++ Week 2/Q5.java
@@ -0,0 +1,14 @@
+public class Question25{ 
+    public static void main(String[] args) { 
+    
+
+//Declare variable with name 'nptel', 'space' and 'java' and proper datatype.
+String nptel = "NPTEL";
+String space = " ";
+String java = "JAVA";
+
+//Initialize the variables with proper input
+
+     System.out.print(nptel+space+java);
+   }
+}
