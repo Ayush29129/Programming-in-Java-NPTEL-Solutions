4457acca0c5ae817818dc7eca735e4cb2ffe0141
Ayush Gupta <48252204+Ayush29129@users.noreply.github.com>
2020-03-27 00:31:24 +0530

    Create Q3.java

---
+++ Week 1/Q3.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Exercise1_3 {
+       public static void main(String[] args) {
+	   Scanner sc = new Scanner(System.in);
+	   int n=sc.nextInt();
+	   int sum=0;
+//Use for or while loop do the operation
+
+for(int i=0;i<=n*2;i=i+2)
+{
+  if(i%3==0)
+    sum = sum+i;
+    
+}
+System.out.print(sum);
+ }
+}
