3ee0c50382262e299e110988508a01734eb9ef91
Ayush Gupta <48252204+Ayush29129@users.noreply.github.com>
2020-04-02 15:36:54 +0530

    Create Q1.java

---
+++ Week 6/Q1.java
@@ -0,0 +1,16 @@
+
+import java.lang.Thread;
+public class Question61 extends Thread{ 
+
+  public void run(){
+  	System.out.print("Thread is Running.");
+  }
+	public static void main(String args[]){  
+
+		// Creating object of thread class
+		Question61 thread=new Question61();  
+
+                // Start the thread
+		thread.start();
+	}  
+}
